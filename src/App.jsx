8b3797102990390a3328c7d24d034f1873403bb4
Joy <github.joysaha@gmail.com>
2024-06-25 09:32:49 +0600

    redirect path created and validate protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import React, {lazy} from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-const Home = lazy(() => import("./pages/Home"))
-const Login = lazy(() => import("./pages/Login"))
-const Chat = lazy(() => import("./pages/Chat"))
-const Groups = lazy(() => import("./pages/Groups"))
+import React, { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
+let user = true;
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/chat/:chatId' element={<Chat></Chat>}></Route>
-        <Route path='/groups' element={<Groups></Groups>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
+        <Route element={<ProtectRoute user={user}></ProtectRoute>}>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/chat/:chatId" element={<Chat></Chat>}></Route>
+          <Route path="/groups" element={<Groups></Groups>}></Route>
+        </Route>
+        <Route
+          path="/login"
+          element={
+            <ProtectRoute user={!user} redirect="/">
+              <Login></Login>
+            </ProtectRoute>
+          }
+        ></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </BrowserRouter>
-   
-  )
-}
+  );
+};
 
-export default App
+export default App;
