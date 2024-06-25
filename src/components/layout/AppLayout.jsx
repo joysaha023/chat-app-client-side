f91a260bbb24a806f17b26f91b2fd89ef4a07c9f
Joy <github.joysaha@gmail.com>
2024-06-25 11:45:15 +0600

    navigate and hedaer navbar route create

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -11,14 +11,36 @@ const AppLayout = () => (WrappedComponent) => {
         <Header></Header>
 
         <Grid container height={"calc(100vh - 4rem)"}>
-          <Grid item xs={4} height={"100%"} bgcolor="primary.main">First</Grid>
-          <Grid item xs={4} height={"100%"} bgcolor="primary.main"> <WrappedComponent {...props}></WrappedComponent></Grid>
-          <Grid item xs={4} height={"100%"} bgcolor="primary.main">Third</Grid>
+          <Grid
+            item
+            sm={4}
+            md={3}
+            sx={{ display: { xs: "noon", sm: "block" } }}
+            height={"100%"}
+          >
+            First
+          </Grid>
+          <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
+            {" "}
+            <WrappedComponent {...props}></WrappedComponent>
+          </Grid>
+          <Grid
+            item
+            md={4}
+            lg={3}
+            sx={{
+              display: { xs: "noon", md: "block" },
+              padding: "2rem",
+              bgcolor: "rgba(0, 0, 0, 0.85)",
+            }}
+            height={"100%"}
+          >
+            Third
+          </Grid>
         </Grid>
-       
-     </>
-);
-  }
+      </>
+    );
+  };
 };
 
 export default AppLayout;
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,87 @@
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { orange } from "../../constants/color";
+import {
+  Add as AddIcon,
+  Menu as MenuIcon,
+  Search as SearchIcon,
+  Group as GroupIcon
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+
+
 
 const Header = () => {
-  return <div>Header</div>;
+
+    const navigate = useNavigate();
+
+  const handleMobile = () => {
+    console.log("Mobile");
+  };
+
+  const openSearchDialog = () => {
+    console.log("search");
+  };
+  const openNewGroup = () => {
+    console.log("openNewGroup");
+  };
+ 
+  const navigateToGroup = () => navigate("/groups")
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }} height={"4rem"}>
+        <AppBar position="static" sx={{ bgcolor: orange }}>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              sx={{ display: { xs: "none", sm: "block" } }}
+            >
+              Chat App
+            </Typography>
+            <Box
+              sx={{
+                display: { xs: "block", sm: "none" },
+              }}
+            >
+              <IconButton color="inherit" onClick={handleMobile}>
+                <MenuIcon></MenuIcon>
+              </IconButton>
+            </Box>
+            <Box sx={{ flexGrow: 1 }}></Box>
+            <Box>
+              <Tooltip title="Search">
+                <IconButton
+                  color="inherit"
+                  size="large"
+                  onClick={openSearchDialog}
+                >
+                  <SearchIcon></SearchIcon>
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="New Group">
+                <IconButton color="inherit" size="large" onClick={openNewGroup}>
+                  <AddIcon></AddIcon>
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Manage Groups">
+              <IconButton color="inherit" size="large" onClick={navigateToGroup}>
+                  <GroupIcon></GroupIcon>
+                </IconButton>
+              </Tooltip>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
 };
 
 export default Header;
