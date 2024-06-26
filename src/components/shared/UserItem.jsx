8dcb29df58cb8c06ffe91c4a20d0ebc04d7ca5ac
Joy <github.joysaha@gmail.com>
2024-06-26 21:33:51 +0600

    design changes and push

--- src/components/shared/UserItem.jsx
+++ src/components/shared/UserItem.jsx
@@ -1,8 +1,8 @@
 import { Avatar, IconButton, ListItem, Stack, Typography } from "@mui/material";
 import React, { memo } from "react";
-import { Add as AddIcon } from "@mui/icons-material";
+import { Add as AddIcon, Remove as RemoveIcon } from "@mui/icons-material";
 
-const UserItem = ({ user, handler, handlerIsLoading }) => {
+const UserItem = ({ user, handler, handlerIsLoading, isAdded = false }) => {
   const { name, _id, avatar } = user;
 
   return (
@@ -23,7 +23,7 @@ const UserItem = ({ user, handler, handlerIsLoading }) => {
             WebkitBoxOrient: "vertical",
             overflow: "hidden",
             textOverflow: "ellipsis",
-            width: "100%"
+            width: "100%",
           }}
         >
           {name}
@@ -31,16 +31,16 @@ const UserItem = ({ user, handler, handlerIsLoading }) => {
         <IconButton
           size="small"
           sx={{
-            bgcolor: "primary.main",
+            bgcolor: isAdded ? "error.main" : "primary.main",
             color: "white",
             "&:hover": {
-              bgcolor: "primary.dark",
+              bgcolor: isAdded ? "error.dark" : "primary.dark",
             },
           }}
           onClick={() => handler(_id)}
           disabled={handlerIsLoading}
         >
-          <AddIcon></AddIcon>
+          {isAdded ? <RemoveIcon></RemoveIcon> : <AddIcon></AddIcon>}
         </IconButton>
       </Stack>
     </ListItem>
