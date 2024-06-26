20c8c94090200a777fd0bc3ae4a1b235e4bb1326
Joy <github.joysaha@gmail.com>
2024-06-26 11:31:04 +0600

    serach bar design and user show

--- src/components/specific/Search.jsx
+++ src/components/specific/Search.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import {
+  Dialog,
+  DialogTitle,
+  InputAdornment,
+  List,
+  ListItem,
+  ListItemText,
+  Stack,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useInputValidation } from "6pp";
+import { Search as SearchIcon } from "@mui/icons-material";
+import UserItem from "../shared/UserItem";
+import { sampleUsers } from "../../constants/sampleData";
+
+const users = [1, 2, 3];
 
 const Search = () => {
-  return (
-    <div>
-      Search
-    </div>
-  )
-}
+  const search = useInputValidation("");
 
-export default Search
+  let isLoadingSendFriendRequest = false;
+  const [users, setUsers] = useState(sampleUsers);
+
+  const addFriendHandler = (id) => {
+    console.log(id);
+  }
+
+  return (
+    <Dialog open>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant="outlined"
+          size="small"
+          inputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon></SearchIcon>
+              </InputAdornment>
+            ),
+          }}
+        ></TextField>
+        <List>
+          {users.map((i) => (
+            <UserItem
+              user={i}
+              key={i._id}
+              handler={addFriendHandler}
+              handlerIsLoading={isLoadingSendFriendRequest}
+            ></UserItem>
+          ))}
+        </List>
+      </Stack>
+    </Dialog>
+  );
+};
+
+export default Search;
--- src/constants/sampleData.js
+++ src/constants/sampleData.js
@@ -14,3 +14,16 @@ export const sampleChats = [
     membars: ["1", "2"],
   },
 ];
+
+export const sampleUsers = [
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "John Doe",
+    _id: "1",
+  },
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "John Khan",
+    _id: "2",
+  },
+];
